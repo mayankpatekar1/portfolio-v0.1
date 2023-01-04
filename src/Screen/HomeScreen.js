1f7c3cd157847ae1319eacf235a43dfbb0015016
mayankpatekar1 <mayankpatekar11234@gmail.com>
2023-01-04 22:24:45 +0530

    NavBar Creation

---
+++ src/Screen/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from '../Section/Home';
+
+export default function HomeScreen(){
+    return(
+        <>
+            <Home />
+            
+        </>
+    )
+}
